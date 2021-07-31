bcfb109bab3656463845c87b3cade219c463278b
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-07-31 17:16:53 +0300

    first commit

---
+++ boot.java
@@ -0,0 +1,19 @@
+import java.io.Console;
+
+public class StartsWithZ {
+    public static void main(String[] args) {
+        Console console = System.console();
+
+        System.out.println("What's your name?");
+        String userName = console.readLine();
+
+        if ( userName.startsWith("Z") ) {
+            System.out.println("Your name starts with a Z!");
+        } else {
+            System.out.println("Your name doesn't start with a Z :(");
+        }
+    }
+}
+
+
+    git remote add origin https://github.com/Becka7/cipher.git
